3df7e654aeebe56da31ced2921edd9d936063125
dagaben <gabriel.stevko@gmail.com>
2026-08-05 14:18:23 +0200

    World river core: spine facade, owner, context cache, gameplay, relationship

---
+++ src/world/worldRiverSpine.ts
@@ -0,0 +1,31 @@
+/** Public river ownership facade: geometry primitives plus explicit generated products. */
+export * from "./riverSpineGeometry";
+export * from "./worldRiverOwner";
+export * from "./worldRiverContextCache";
+export {
+  DEFAULT_RIVER_GENERATION_CONFIG,
+  PROCEDURAL_RIVER_GENERATION_VERSION,
+  WORLD_RIVER_CONTROL_POINTS,
+  authoredR6RiverSpine,
+  generateMacroControlPoints,
+  generateMeanderedControlPoints,
+  generateMeanderRegions,
+  sampleRegionalMeanderStrength,
+  validateControlPolygonSeparation,
+  validateSmoothedSpineSeparation,
+  getWorldRiverGeneration,
+  resetWorldRiverGenerationCaches,
+  worldRiverGeneration,
+  worldRiverGenerationCacheSize,
+  worldRiverMacroSpine,
+  worldRiverSpine,
+  referenceWorldRiverGeneration,
+  referenceWorldRiverMacroSpine,
+  referenceWorldRiverSpine,
+  type MacroRiverGeneration,
+  type RiverGenerationConfig,
+  type RiverGenerationMode,
+  type MeanderRegion,
+  type MeanderRegionProfile,
+  type RegionalSuitability,
+} from "./worldRiverGeneration";
