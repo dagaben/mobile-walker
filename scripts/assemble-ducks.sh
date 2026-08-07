03d61ed02db981acfd6a28ab6795ac8baffc5db2
dagaben <gabriel.stevko@gmail.com>
2026-08-07 08:46:41 +0200

    Update ducks assembler for 3 parts

--- scripts/assemble-ducks.sh
+++ scripts/assemble-ducks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
-cat scripts/ducks_a.txt scripts/ducks_b.txt > src/game/ducks.ts
+cat scripts/ducks_a.txt scripts/ducks_b1.txt scripts/ducks_b2.txt > src/game/ducks.ts
 echo "assembled ducks.ts $(wc -c < src/game/ducks.ts) bytes"
